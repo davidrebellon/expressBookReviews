26a2959db184311acd72903dd60e0208fd6d19ad
davidrebellon <david1rebellon@gmail.com>
2026-02-06 10:38:29 -0500

    implement promise and callbacks for general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,16 +1,9 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
-const parsedBooks = new Promise((resolve, reject) => {
-  try {
-    resolve(books);
-  } catch (err) {
-    reject(err);
-  }
-});
 
 public_users.post("/register", (req, res) => {
   try {
@@ -31,10 +24,15 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', async function (req, res) {
+public_users.get('/', function (req, res) {
   try {
-    const books = await parsedBooks;
-    return res.status(200).json(books);
+    new Promise((resolve, reject) => {
+      resolve(books);
+    }).then((books) => {
+      return res.status(200).json(books);
+    }).catch((err) => {
+      return res.status(500).json({ message: "Internal Server Error while getting book list" });
+    });
   } catch (err) {
     return res.status(500).json({ message: "Internal Server Error while getting book list" });
   }
@@ -43,17 +41,22 @@ public_users.get('/', async function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
   try {
-    const isbn = req.params?.isbn;
-    const books = await parsedBooks;
-    if (!isbn) {
-      // no ISBN provided
-      return res.status(400).json({ message: "ISBN parameter is required" });
-    } else if (books[isbn]) {
-      // ISBN found among books
-      return res.status(200).json(books[isbn]);
-    } else {
-      return res.status(404).json({ message: "Book not found" });
-    }
+    new Promise((resolve, reject) => {
+      const isbn = req.params?.isbn;
+      if (!isbn) {
+        // no ISBN provided
+        reject(res.status(400).json({ message: "ISBN parameter is required" }));
+      } else if (books[isbn]) {
+        // ISBN found among books
+        resolve(books[isbn]);
+      } else {
+        reject(res.status(404).json({ message: "Book not found" }));
+      }
+    }).then((book) => {
+      return res.status(200).json(book);
+    }).catch((err) => {
+      return res.status(500).json({ message: "Internal Server Error while getting book by ISBN" });
+    });
   } catch (err) {
     return res.status(500).json({ message: "Internal Server Error while getting book by ISBN" });
   }
@@ -62,14 +65,17 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
   try {
-    const author = req.params?.author;
-    console.log("Author requested:", author);
-    const books = await parsedBooks;
-    if (!author) {
-      // no author provided
-      return res.status(400).json({ message: "Author parameter is required" });
-    } else {
-      // search for books by the given author
+    new Promise((resolve, reject) => {
+      const author = req.params?.author;
+      console.log("Author requested:", author);
+      if (!author) {
+        // no author provided
+        reject(res.status(400).json({ message: "Author parameter is required" }));
+      } else {
+        // search for books by the given author
+        resolve(books);
+      }
+    }).then((books) => {
       const results = [];
       for (const [isbn, book] of Object.entries(books)) {
         console.log(book.author, author);
@@ -83,8 +89,9 @@ public_users.get('/author/:author', async function (req, res) {
       } else {
         return res.status(404).json({ message: "No books found by the specified author" });
       }
-    }
-
+    }).catch((err) => {
+      return res.status(500).json({ message: "Internal Server Error while getting books by author" });
+    });
   } catch (err) {
     return res.status(500).json({ message: "Internal Server Error while getting books by author" });
   }
@@ -93,13 +100,17 @@ public_users.get('/author/:author', async function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
   try {
-    const title = req.params?.title;
-    console.log("Title requested:", title);
-    const books = await parsedBooks;
-    if (!title) {
-      // no title provided
-      return res.status(400).json({ message: "Title parameter is required" });
-    } else {
+    new Promise((resolve, reject) => {
+      const title = req.params?.title;
+      console.log("Title requested:", title);
+      if (!title) {
+        // no title provided
+        reject(res.status(400).json({ message: "Title parameter is required" }));
+      } else {
+        resolve(books);
+      }
+    }).then((books) => {
+      const title = req.params?.title;
       // search for books by the given title
       const results = [];
       for (const [isbn, book] of Object.entries(books)) {
@@ -114,8 +125,9 @@ public_users.get('/title/:title', async function (req, res) {
       } else {
         return res.status(404).json({ message: "No books found by the specified author" });
       }
-    }
-
+    }).catch((err) => {
+      return res.status(500).json({ message: "Internal Server Error while getting books by title" });
+    });
   } catch (err) {
     return res.status(500).json({ message: "Internal Server Error while getting books by title" });
   }
@@ -124,16 +136,23 @@ public_users.get('/title/:title', async function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   try {
-    const isbn = req.params?.isbn;
-    if (!isbn) {
-      // no ISBN provided
-      return res.status(400).json({ message: "ISBN parameter is required" });
-    } else if (books[isbn]) {
-      // ISBN found among books
-      return res.status(200).json(books[isbn].reviews);
-    } else {
-      return res.status(404).json({ message: "Book not found" });
-    }
+    new Promise((resolve, reject) => {
+      const isbn = req.params?.isbn;
+      if (!isbn) {
+        // no ISBN provided
+        reject(res.status(400).json({ message: "ISBN parameter is required" }));
+      } else if (books[isbn]) {
+        // ISBN found among books
+        resolve(books[isbn].reviews);
+      } else {
+        reject(res.status(404).json({ message: "Book not found" }));
+      }
+    }).then((reviews) => {
+      return res.status(200).json(reviews);
+    }).catch((err) => {
+      console.log(err);
+      return res.status(500).json({ message: "Internal Server Error while getting books review" });
+    });
   } catch (err) {
     return res.status(500).json({ message: "Internal Server Error while getting books review" });
   }
